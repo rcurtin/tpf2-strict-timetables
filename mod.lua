83283b7bec3ba5cf176bf1bc2f28c3102ce95841
Ryan Curtin <ryan@ratml.org>
2025-11-27 14:06:37 -0500

    Add basic mod description.

---
+++ mod.lua
@@ -0,0 +1,20 @@
+function data()
+  return {
+    info = {
+      name = "Strict Timetables",
+      description = "Strict Timetables allows specific and strict departure times to be set for vehicles on an entire line.",
+      minorVersion = 0,
+      severityAdd = "NONE",
+      severityRemove = "WARNING",
+      params = { },
+      tags = { "Script Mod", "Script", "Timetable", "Timetables" },
+      authors = {
+        {
+          name = "Ryan Curtin",
+          role = "CREATOR",
+          tfnetId = ""
+        },
+      },
+    },
+  }
+end
